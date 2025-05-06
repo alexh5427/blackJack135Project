040f87ce6dbaba504f42a2e80a3d670d6a22d61d
alexh5427 <alexander.m.higgins@maine.edu>
2025-05-06 18:44:32 -0400

    Update game.h

--- game.h
+++ game.h
@@ -3,13 +3,21 @@
 #include "deck.h"
 #define MAX_HAND_CARDS 12
 
+typedef struct{
+    Card *cards;
+    int count;
+} Hand;
 
+void startGame(Hand *player, Hand *dealer);
 
+int playRound(Hand *player, Hand *dealer, Card *deck);
 
+int findTotal(Hand *hand);
 
+void showHand(const char *name, Hand *hand, int hidedealercard);
 
+void dealerSteve(Hand *dealer, Card *deck, int *deckIndex);
 
-
-
+int checkWinner(Hand *player, Hand *dealer);
 
 #endif
