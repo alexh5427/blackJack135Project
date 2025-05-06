2fbd0923c990b2c9c3b8b40367060488da05bae2
alexh5427 <alexander.m.higgins@maine.edu>
2025-05-06 17:19:45 -0400

    Create deck.c

---
+++ deck.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "deck.h"
+
+Card* createDeck(){
+    Card* deck = malloc (DECK_SIZE * sizeof(Card));
+    if (deck == NULL){
+        fprintf(stderr, "FAILED!\n");
+        return NULL;
+    }
+    Card *cardPtr = deck;
+    for (int suit = 0; suit < 4; ++suit){
+        for (int rank = 1; rank <=13; ++rank){
+            cardPtr->rank = rank;
+            cardPtr->suit = suit;
+            if (rank == 1){
+                cardPtr->value = 11;
+            } else if (rank >= 10){
+                cardPtr->value = 10;
+            } else {
+                cardPtr->value = rank;
+            }
+            cardPtr++;
+        }
+    }
+    return deck;
+}
+
+
+void shuffleDeck(Card* deck){
+    if (deck == NULL){
+        return;
+    }
+    for (int i = DECK_SIZE - 1; i > 0; --i){
+        int j = rand() % (i+1);
+        Card temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
+
+
+Card drawCard(Card* deck, int* deckIndex){
+    Card card = deck[*deckIndex];
+    (*deckIndex)++;
+    return card;
+}
+
+void freeDeck(Card* deck){
+    free(deck);
+}
