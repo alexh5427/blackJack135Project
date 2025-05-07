39106bcaba2ef13e4e911148dcd284baf3f20735
alexh5427 <alexander.m.higgins@maine.edu>
2025-05-06 21:48:05 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,36 @@
 #include <time.h>
 #include "deck.h"
 #include "game.h"
-#include "settings.h"
+
 
 int main(){
     srand((unsigned int) time(NULL));
 
     Card *deck = createDeck();
-    if (deck == NULL){
+    if (!deck){
         return 1;
     }
+    shuffleDeck(deck);
+    Hand player, dealer;
+    startGame(&player, &dealer);
+    int running=1;
+    while (running){
+        printf("\n1.Play\n2.Quit\nChoose: ");
+        int choice;
+        scanf(" %d",&choice);
+        if (choice==1){
+            shuffleDeck(deck);
+            playRound(&player,&dealer,deck);
+        }else if (choice==2){
+            running=0;
+        }else{
+            printf("try again");
+        }
+    }
+    free(player.cards);
+    free(dealer.cards);
+    freeDeck(deck);
+    return 0;
 
 
 
